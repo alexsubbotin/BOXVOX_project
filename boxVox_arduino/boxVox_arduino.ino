9c530447700509e8e58e05bcfca04f9bee06714a
Alexey Subbotin <subbotin910@gmail.com>
2018-05-07 21:02:42 +0500

    all done

--- boxVox_arduino/boxVox_arduino.ino
+++ boxVox_arduino/boxVox_arduino.ino
@@ -1,4 +1,6 @@
 // MY PROJECT
+#include <Thread.h>  // подключение библиотеки ArduinoThread
+#include <Tone.h>
 #define soundPin1 13 // пищалка 1
 #define svet1 12 // светодиод 1
 #define echoPin1 11 // (дальномер 1) куда посылается сигнал 
@@ -10,6 +12,28 @@
 #define echoPin2 6 // (дальномер 2) куда посылается сигнал 
 #define trigPin2 5 // (дальномер 2) податся сигнал, который потом преобразуется и посылается
 #define button2 4 // кнопка переключатель октав 2
+
+Tone tone1, tone2;
+
+
+// массив частот нот
+int notes[2][13] = {{261, 277, 293, 311, 329, 349, 370, 392, 415, 440, 466, 494, 523}, // первая октава
+                    {523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 988, 523}}; // вторая октава
+
+String notesNames[] = {"C", "C#", "D", "D#", "E", "F", "F#", "F", "G", "G#", "A", "B", "H", "C"}; // названия нот
+int duration1, cm1, duration2, cm2; // нужны для считывания см
+
+int currentOctave1 = 0; // октава в настоящий момент времени
+int currentNote1 = -1; // нота в настоящий момент времени
+int currentOctave2 = 1; // октава в настоящий момент времени
+int currentNote2 = -1; // нота в настоящий момент времени
+
+int del = 25;
+
+String message; // сообщение 
+
+Thread thread1 = Thread(); // создаём поток управления светодиодом
+Thread thread2 = Thread(); // создаём поток управления сиреной
 
 void setup() {
   pinMode(soundPin1, OUTPUT);
@@ -23,34 +47,35 @@ void setup() {
   pinMode(echoPin2, INPUT);
   pinMode(button2, INPUT);
   pinMode(svet2, OUTPUT);
+
+  /*thread1.onRun(sound1);     // назначаем потоку задачу
+  thread1.setInterval(10); // задаём интервал срабатывания, мсек
+
+  thread2.onRun(sound2);     // назначаем потоку задачу
+  thread2.setInterval(10); // задаём интервал срабатывания, мсек*/
+
+
+  tone1.begin(soundPin1);
+  tone2.begin(soundPin2);
   
   Serial.begin(9600);
 }
 
-// массив частот нот
-int notes[2][13] = {{261, 277, 293, 311, 329, 349, 370, 392, 415, 440, 466, 494, 523}, // первая октава
-                    {523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 988, 523}}; // вторая октава
+/*void sound1(){
+  //noTone(soundPin2);
+  tone(soundPin1, notes[0][13]);
+  //delay(10);
+}
 
-String notesNames[] = {"C", "C#", "D", "D#", "E", "F", "F#", "F", "G", "G#", "A", "B", "H", "C"}; // названия нот
-int duration1, cm1, duration2, cm2; // нужны для считывания см
-
-int currentOctave1 = 0; // октава в настоящий момент времени
-int currentNote1 = 1; // нота в настоящий момент времени
-int currentOctave2 = 1; // октава в настоящий момент времени
-int currentNote2 = 0; // нота в настоящий момент времени
-
-String message; // сообщение 
+void sound2(){
+  //noTone(soundPin1);
+  tone(soundPin2, notes[1][8]);
+  //delay(10);
+}*/
 
 void loop() {
-  // ПЕРВАЯ РУКА
-  digitalWrite(trigPin1, LOW); // удаляем сигнал на 2млсек
-  delayMicroseconds(2); 
-  digitalWrite(trigPin1, HIGH); // принимаем 10млсек сигнал
-  delayMicroseconds(10);
-  digitalWrite(trigPin1, LOW); // убиарем после 10 млсек
-  duration1 = pulseIn(echoPin1, HIGH); // палсиен снимает показания с эхопина (длина сигнала)
-  cm1 = duration1 / 58; // получаем величину в см
 
+        
   if (digitalRead(button1)) // меняем октаву по нажатию кнопки
         delay(50);
         if (digitalRead(button1)){
@@ -60,54 +85,80 @@ void loop() {
             currentOctave1 = 1;
         }
 
-  if(cm1 > 65 || cm1 < 0){
-    noTone(soundPin1); // не звучит, если выходит за диапазон 
-    digitalWrite(svet1, LOW); // свет не горит
-    currentNote1 = -1; // никакая нота не играет
-  }
-  else{
-      tone(soundPin1, notes[currentOctave1][cm1 / 5]); // играет нота
-      digitalWrite(svet1, HIGH); // горит свет
-      currentNote1 = cm1 / 5; // запоминаем ноту
-    }
-
-  message = "1." + (String)currentNote1 + "." + (String)currentOctave1 + ".";
-  Serial.println(message); // отправяем в форму
-  delay(20);
-
-  // ВТОРАЯ РУКА
-  /*digitalWrite(trigPin2, LOW); // удаляем сигнал на 2млсек
-  delayMicroseconds(2); 
-  digitalWrite(trigPin2, HIGH); // принимаем 10млсек сигнал
-  delayMicroseconds(10);
-  digitalWrite(trigPin2, LOW); // убиарем после 10 млсек
-  duration2 = pulseIn(echoPin1, HIGH); // палсиен снимает показания с эхопина (длина сигнала)
-  cm2 = duration1 / 58; // получаем величину в см
-
-  if (digitalRead(button2)) // меняем октаву по нажатию кнопки
+     
+       if (digitalRead(button2)) // меняем октаву по нажатию кнопки
         delay(50);
         if (digitalRead(button2)){
           if(currentOctave2 == 1)
             currentOctave2 = 0;
           else
             currentOctave2 = 1;
-        }
+        } 
+
+          
+  // ПЕРВАЯ РУКА
+  digitalWrite(trigPin1, LOW); // удаляем сигнал на 2млсек
+  delayMicroseconds(2); 
+  digitalWrite(trigPin1, HIGH); // принимаем 10млсек сигнал
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW); // убиарем после 10 млсек
+  //Serial.println("жду 1");
+  duration1 = pulseIn(echoPin1, HIGH); // палсиен снимает показания с эхопина (длина сигнала)
+  //Serial.println("принял 1");
+  cm1 = duration1 / 58; // получаем величину в см
+
+
+  if(cm1 > 65 || cm1 < 0){
+    //noTone(soundPin1); // не звучит, если выходит за диапазон 
+    tone1.stop();
+    digitalWrite(svet1, LOW); // свет не горит
+    currentNote1 = -1; // никакая нота не играет
+  }
+  else{
+      //tone(soundPin1, notes[currentOctave1][cm1 / 5]); // играет нота
+      tone1.play(notes[currentOctave1][cm1 / 5]);
+      digitalWrite(svet1, HIGH); // горит свет
+      currentNote1 = cm1 / 5; // запоминаем ноту
+    }
+
+  message = "1." + (String)currentNote1 + "." + (String)currentOctave1 + ".";
+  Serial.println(message); // отправяем в форму
+  //delay(del);
+
+  // ВТОРАЯ РУКА
+  digitalWrite(trigPin2, LOW); // удаляем сигнал на 2млсек
+  delayMicroseconds(2); 
+  digitalWrite(trigPin2, HIGH); // принимаем 10млсек сигнал
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW); // убиарем после 10 млсек
+  //Serial.println("жду 2");
+  duration2 = pulseIn(echoPin2, HIGH); // палсиен снимает показания с эхопина (длина сигнала)
+  //Serial.println("принял 2");
+  cm2 = duration2 / 58; // получаем величину в см
+
+
 
   if(cm2 > 65 || cm2 < 0){
-    noTone(soundPin2); // не звучит, если выходит за диапазон 
+    //noTone(soundPin2); // не звучит, если выходит за диапазон 
+    tone2.stop();
     digitalWrite(svet2, LOW); // свет не горит
     currentNote2 = -1; // никакая нота не играет
   }
   else{
-      tone(soundPin2, notes[currentOctave2][cm2 / 5]); // играет нота
+      //tone(soundPin2, notes[currentOctave2][cm2 / 5]); // играет нота
+      tone2.play(notes[currentOctave2][cm2 / 5]);
       digitalWrite(svet2, HIGH); // горит свет
       currentNote2 = cm2 / 5; // запоминаем ноту
-    }*/
+    }
 
   message = "2." + (String)currentNote2 + "." + (String)currentOctave2 + ".";
   Serial.println(message); // отправяем в форму
-  delay(20);
+  delay(del*2);
+  
+
 }
+
+
 
 
 
